feedf850aef973d54a1549af9e0185884fc615bc
BayuuOFC <nandoardipradana123@gmail.com>
2022-03-26 11:33:49 +0700

    BAYUBOT-MD

---
+++ bahasa/ind.js
@@ -0,0 +1,17 @@
+exports.onlygroup = () => {
+return`Fitur Hanya Bisa Digunakan Di Group`
+}
+
+exports.wait = () => {
+return `Mohon Tunggu Sebentar..`
+}
+
+exports.owneronly = () => {
+return `Menu Hanya Bisa Digunakan Khusus Owner`
+}
+exports.adminonly = () =>{
+return `Menu Hanya Bisa Digunakan Oleh Admin Group!`
+}
+exports.botAdmin = () =>{
+return `Jadikan Bot Sebagai Admin Untuk Menggunakan Fitur Ini`
+}
---
+++ config.js
@@ -0,0 +1,51 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+	zhir: 'https://zahirr-web.herokuapp.com',
+	lol: 'https://api.lolhuman.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'apikeylu',
+	'https://zahirr-web.herokuapp.com': 'zahirgans',
+	'https://api.lolhuman.xyz': 'apikeylu',
+}
+
+// Other
+global.owner = ['6281285866502', '6281398003097']
+global.botname = 'BAYUBOT-MD'
+global.ownername = 'BAYU OFC'
+global.packname = 'BAYUBOT-MD'
+global.author = 'CREATED BY BAYU OFC'
+global.sessionName = 'session'
+global.prefa = ['#','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done !! Subscribe BAYU OFC',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Mohon Tunggu Sebentar..',
+}
+global.thumb = fs.readFileSync('./lib/bayu.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
